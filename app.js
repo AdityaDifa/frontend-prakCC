3f7357d2ebf1fa6853e67e5b73b8b5e8b30f893d
Aditya Difa Amanullah <adityadifa123@gmail.com>
2024-05-27 21:59:47 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// app.js
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-
 // Serve static files (opsional, jika kamu memiliki file-file statis di folder lain)
-app.use(express.static(path.join(__dirname, 'auth')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html on the root route
+// Serve index.html pada root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html')); // Sesuaikan dengan lokasi index.html yang sebenarnya
 });
+
+// Mengizinkan akses ke authentication.php tanpa melakukan pengecekan
+app.use('/auth', express.static(path.join(__dirname, 'auth/authentication.php')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
