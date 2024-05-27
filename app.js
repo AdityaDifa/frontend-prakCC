e6fb8d816edead91b939014a8a434be530dda128
Aditya Difa Amanullah <adityadifa123@gmail.com>
2024-05-27 21:44:03 +0700

    gnti app js

--- app.js
+++ app.js
@@ -2,15 +2,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const connection = require('./db'); // Hanya jika Anda masih membutuhkan koneksi database
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
-
-// Set the static files location
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html on the root route
 app.get('/', (req, res) => {
